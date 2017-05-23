6667ff2b8334b5e848acde3158a5fffb121048a1
Norbert de Langen <ndelangen@me.com>
2017-05-23 20:56:54 +0200

    FIX a few things

--- addons/info/example/story.js
+++ addons/info/example/story.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { storiesOf, action } from '@storybook/react';
+import { storiesOf } from '@storybook/react';
 import { action } from '@storybook/addon-actions';
 import backgrounds from 'react-storybook-addon-backgrounds';
 
