storybookjs/react-native

Deprecate built-in addons
Use `action` and `linkTo` addons just like any other storybook addon.
Update examples, docs, and add deprecation warning to code.

examples/cra-storybook/src/stories/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
-import { storiesOf, action, linkTo } from '@kadira/storybook';
+import { storiesOf } from '@kadira/storybook';
+import { action } from '@kadira/storybook-addon-actions';
+import { linkTo } from '@kadira/storybook-addon-links';
 import Button from './Button';
 import Welcome from './Welcome';
 

packages/getstorybook/generators/REACT_NATIVE/template/storybook/stories/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { Text } from 'react-native';
-import { storiesOf, action, linkTo } from '@kadira/react-native-storybook';
+import { storiesOf } from '@kadira/react-native-storybook';
+import { action } from '@kadira/storybook-addon-actions';
+import { linkTo } from '@kadira/storybook-addon-links';
 
 import Button from './Button';
 import CenterView from './CenterView';

packages/react-native-storybook/src/index.js
@@ -1,3 +1,4 @@
+import deprecate from 'util-deprecate';
 import Preview from './preview';
 
 const preview = new Preview();
@@ -10,5 +11,15 @@ export const getStorybook = preview.getStorybook.bind(preview);
 export const getStorybookUI = preview.getStorybookUI.bind(preview);
 
 // NOTE export these to keep backwards compatibility
-export { action } from '@kadira/storybook-addon-actions';
-export { linkTo } from '@kadira/storybook-addon-links';
+import { action as _action } from '@kadira/storybook-addon-actions';
+import { linkTo as _linkTo } from '@kadira/storybook-addon-links';
+
+export const action = deprecate(
+  _action,
+  '@kadira/storybook action is deprecated. See: https://github.com/storybooks/storybook/tree/master/packages/addon-actions',
+);
+
+export const linkTo = deprecate(
+  _linkTo,
+  '@kadira/storybook linkTo is deprecated. See: https://github.com/storybooks/storybook/tree/master/packages/addon-links',
+);

packages/react-storybook/src/client/index.js
@@ -1,3 +1,4 @@
+import deprecate from 'util-deprecate';
 import * as previewApi from './preview';
 
 export const storiesOf = previewApi.storiesOf;
@@ -7,5 +8,15 @@ export const configure = previewApi.configure;
 export const getStorybook = previewApi.getStorybook;
 
 // NOTE export these to keep backwards compatibility
-export { action } from '@kadira/storybook-addon-actions';
-export { linkTo } from '@kadira/storybook-addon-links';
+import { action as _action } from '@kadira/storybook-addon-actions';
+import { linkTo as _linkTo } from '@kadira/storybook-addon-links';
+
+export const action = deprecate(
+  _action,
+  '@kadira/storybook action is deprecated. See: https://github.com/storybooks/storybook/tree/master/packages/addon-actions',
+);
+
+export const linkTo = deprecate(
+  _linkTo,
+  '@kadira/storybook linkTo is deprecated. See: https://github.com/storybooks/storybook/tree/master/packages/addon-links',
+);

packages/storyshots/stories/required_with_context/Button.stories.js
@@ -1,5 +1,7 @@
 import React from 'react';
-import { storiesOf, action, linkTo } from '@kadira/storybook';
+import { storiesOf } from '@kadira/storybook';
+import { action } from '@kadira/storybook-addon-actions';
+import { linkTo } from '@kadira/storybook-addon-links';
 import Button from './Button';
 import Welcome from './Welcome';
 

packages/storyshots/stories/required_with_context/Welcome.stories.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { storiesOf, linkTo } from '@kadira/storybook';
+import { storiesOf } from '@kadira/storybook';
+import { linkTo } from '@kadira/storybook-addon-links';
 import Welcome from './Welcome';
 
 storiesOf('Welcome', module).add('to Storybook', () => <Welcome showApp={linkTo('Button')} />);

joujiahe/storybook

examples/cra-storybook/src/stories/index.js
@@ -1,5 +1,8 @@
 import React from 'react';
-import { storiesOf, action, linkTo } from '@storybook/storybook';
+import { storiesOf } from '@storybook/storybook';
+import { action } from '@storybook/storybook-addon-actions';
+import { linkTo } from '@storybook/storybook-addon-links';
+
 import Button from './Button';
 import Welcome from './Welcome';
 

packages/getstorybook/generators/REACT_NATIVE/template/storybook/stories/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { Text } from 'react-native';
-import { storiesOf, action, linkTo } from '@storybook/react-native-storybook';
+import { storiesOf } from '@storybook/react-native-storybook';
+import { action } from '@storybook/storybook-addon-actions';
+import { linkTo } from '@storybook/storybook-addon-links';
 
 import Button from './Button';
 import CenterView from './CenterView';

packages/react-native-storybook/src/index.js
@@ -1,3 +1,4 @@
+import deprecate from 'util-deprecate';
 import Preview from './preview';
 
 const preview = new Preview();
@@ -10,5 +11,15 @@ export const getStorybook = preview.getStorybook.bind(preview);
 export const getStorybookUI = preview.getStorybookUI.bind(preview);
 
 // NOTE export these to keep backwards compatibility
-export { action } from '@storybook/storybook-addon-actions';
-export { linkTo } from '@storybook/storybook-addon-links';
+import { action as _action } from '@storybook/storybook-addon-actions';
+import { linkTo as _linkTo } from '@storybook/storybook-addon-links';
+
+export const action = deprecate(
+  _action,
+  '@storybook/storybook action is deprecated. See: https://github.com/storybooks/storybook/tree/master/packages/addon-actions',
+);
+
+export const linkTo = deprecate(
+  _linkTo,
+  '@storybook/storybook linkTo is deprecated. See: https://github.com/storybooks/storybook/tree/master/packages/addon-links',
+);

packages/react-storybook/src/client/index.js
@@ -1,3 +1,4 @@
+import deprecate from 'util-deprecate';
 import * as previewApi from './preview';
 
 export const storiesOf = previewApi.storiesOf;
@@ -7,5 +8,15 @@ export const configure = previewApi.configure;
 export const getStorybook = previewApi.getStorybook;
 
 // NOTE export these to keep backwards compatibility
-export { action } from '@storybook/storybook-addon-actions';
-export { linkTo } from '@storybook/storybook-addon-links';
+import { action as _action } from '@storybook/storybook-addon-actions';
+import { linkTo as _linkTo } from '@storybook/storybook-addon-links';
+
+export const action = deprecate(
+  _action,
+  '@storybook/storybook action is deprecated. See: https://github.com/storybooks/storybook/tree/master/packages/addon-actions',
+);
+
+export const linkTo = deprecate(
+  _linkTo,
+  '@storybook/storybook linkTo is deprecated. See: https://github.com/storybooks/storybook/tree/master/packages/addon-links',
+);

packages/storyshots/stories/required_with_context/Button.stories.js
@@ -1,5 +1,7 @@
 import React from 'react';
-import { storiesOf, action, linkTo } from '@storybook/storybook';
+import { storiesOf } from '@storybook/storybook';
+import { action } from '@storybook/storybook-addon-actions';
+
 import Button from './Button';
 import Welcome from './Welcome';
 

packages/storyshots/stories/required_with_context/Welcome.stories.js
@@ -1,5 +1,7 @@
 import React from 'react';
-import { storiesOf, linkTo } from '@storybook/storybook';
+import { storiesOf } from '@storybook/storybook';
+import { linkTo } from '@storybook/storybook-addon-links';
+
 import Welcome from './Welcome';
 
 storiesOf('Welcome', module).add('to Storybook', () => <Welcome showApp={linkTo('Button')} />);